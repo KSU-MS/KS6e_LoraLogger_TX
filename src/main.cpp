0d94216a9a3ad6a4e689097638451af3fb617869
mathbrook <32876429+mathbrook@users.noreply.github.com>
2022-08-09 23:35:23 -0400

    semi working radio

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,10 @@
 #include <Metro.h>
 #include <FlexCAN_T4.h>
 #include <RadioLib.h>
-#define pin_cs 10
-#define pin_dio0 6
-#define pin_nrst 7
-#define pin_dio1 5
+int pin_cs = 10;
+int pin_dio0 = 6;
+int pin_nrst = 7;
+int pin_dio1 = 5;
 SX1276 radio = new Module(pin_cs, pin_dio0, pin_nrst, pin_dio1);
 /*
  * CAN Variables
@@ -24,6 +24,7 @@ static CAN_message_t msg_rx;
 static CAN_message_t msg_tx;
 // static CAN_message_t xb_msg;
 File logger;
+String output = "7FF12345678,7FF12345678,7FF12345678";
 /*
  * Variables to help with time calculation
  */
@@ -31,6 +32,7 @@ uint64_t global_ms_offset = 0;
 uint64_t last_sec_epoch;
 Metro timer_debug_RTC = Metro(1000);
 Metro timer_flush = Metro(500);
+Metro LoraTimer = Metro(100);
 void parse_can_message();
 void write_to_SD(CAN_message_t *msg);
 time_t getTeensy3Time();
@@ -41,7 +43,9 @@ void setup() {
   Serial.print(F("[SX1276] Initializing ... "));
   //int state = radio.begin(); //-121dBm
   //int state = radio.begin(868.0); //-20dBm
-  int state = radio.beginFSK(915.0,300); //-23dBm
+  int state = radio.beginFSK(915.0,20); //-23dBm
+  radio.disableAddressFiltering();
+
   if (state == RADIOLIB_ERR_NONE) {
     Serial.println(F("init success!"));
   } else {
@@ -62,17 +66,16 @@ void setup() {
   // controlled via two pins (RX enable, TX enable)
   // to enable automatic control of the switch,
   // call the following method
-  int pin_rx_enable = 5;
-  int pin_tx_enable = 6;
+  int pin_rx_enable = 8;
+  int pin_tx_enable = 9;
   radio.setRfSwitchPins(pin_rx_enable, pin_tx_enable);
     //SD logging init stuff
-    delay(5000); // Prevents suprious text files when turning the car on and off rapidly
-    pinMode(LED_BUILTIN,OUTPUT);
+    delay(500); // Prevents suprious text files when turning the car on and off rapidly
     /* Set up Serial, CAN */
     //Serial.begin(115200);
 
     /* Set up real-time clock */
-    //Teensy3Clock.set(1656732300); // set time (epoch) at powerup  (COMMENT OUT THIS LINE AND PUSH ONCE RTC HAS BEEN SET!!!!)
+    Teensy3Clock.set(1656732300); // set time (epoch) at powerup  (COMMENT OUT THIS LINE AND PUSH ONCE RTC HAS BEEN SET!!!!)
     setSyncProvider(getTeensy3Time); // registers Teensy RTC as system time
     if (timeStatus() != timeSet) {
         Serial.println("RTC not set up - uncomment the Teensy3Clock.set() function call to set the time");
@@ -116,20 +119,49 @@ void setup() {
     logger.flush();
 }
 void loop() {
-  digitalWrite(LED_BUILTIN,LOW);
+    if(Serial.available()){
+        output = Serial.readString();
+        
+    }
     /* Process and log incoming CAN messages */
     parse_can_message();
     /* Flush data to SD card occasionally */
-    if (timer_flush.check()) {
-        logger.flush(); // Flush data to disk (data is also flushed whenever the 512 Byte buffer fills up, but this call ensures we don't lose more than a second of data when the car turns off)
-    }
+    // if (timer_flush.check()) {
+    //     logger.flush(); // Flush data to disk (data is also flushed whenever the 512 Byte buffer fills up, but this call ensures we don't lose more than a second of data when the car turns off)
+    // }
     /* Print timestamp to serial occasionally */
     if (timer_debug_RTC.check()) {
         Serial.println(Teensy3Clock.get());
-        CAN_message_t msg_tx;
-        msg_tx.id=0x3FF;
-        CAN.write(msg_tx);
+        // msg_tx.id=0x3FF;
+        // CAN.write(msg_tx);
     }
+
+
+    if(LoraTimer.check()){
+        int state = radio.transmit(output);
+        if (state == RADIOLIB_ERR_NONE) {
+            // the packet was successfully transmitted
+            Serial.println(F(" success!"));
+
+            // print measured data rate
+            Serial.print(F("[SX1276] Datarate:\t"));
+            Serial.print(radio.getDataRate());
+            Serial.println(F(" bps"));
+
+        } else if (state == RADIOLIB_ERR_PACKET_TOO_LONG) {
+            // the supplied packet was longer than 256 bytes
+            Serial.println(F("too long!"));
+
+        } else if (state == RADIOLIB_ERR_TX_TIMEOUT) {
+            // timeout occurred while transmitting packet
+            Serial.println(F("timeout!"));
+
+        } else {
+            // some other error occurred
+            Serial.print(F("failed, code "));
+            Serial.println(state);
+        }
+}
 }
 void parse_can_message() {
     while (CAN.read(msg_rx)) {
@@ -141,7 +173,6 @@ void parse_can_message() {
 void write_to_SD(CAN_message_t *msg) { // Note: This function does not flush data to disk! It will happen when the buffer fills or when the above flush timer fires
     // Calculate Time
     //This block is verified to loop through
-    digitalWrite(LED_BUILTIN,HIGH);
 
     uint64_t sec_epoch = Teensy3Clock.get();
     if (sec_epoch != last_sec_epoch) {
